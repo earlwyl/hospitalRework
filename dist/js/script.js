988004251f7b0b606b3badc18184905c4ff1884e
lieh <akhmd.gibran@gmail.com>
2024-07-27 17:05:35 +0700

    navbar about bussines done

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,15 +1,14 @@
 // navbar fixed
-window.onscroll -
-  function () {
-    const header = document.querySelector("header");
-    const fixednav = header.offsetTop;
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixednav = header.offsetTop;
 
-    if (window.scrollY > fixednav) {
-      header.classList.add("navbar-fixed");
-    } else {
-      header.classList.remove("navbar-fixeed");
-    }
-  };
+  if (window.scrollY > fixednav) {
+    header.classList.add("navbar-fixed");
+  } else {
+    header.classList.remove("navbar-fixeed");
+  }
+};
 
 // hamburger
 const hamburger = document.querySelector("#hamburger");
